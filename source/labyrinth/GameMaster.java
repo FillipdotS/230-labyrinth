e1038c768798e333a8e68ab72c71f57689ef7e03
Matto <kingfok2000@gmail.come>
2020-11-23 00:13:54 +0900

    Fixed some issus

--- source/labyrinth/GameMaster.java
+++ source/labyrinth/GameMaster.java
@@ -1,18 +1,22 @@
 package source.labyrinth;
 
+import java.net.Inet4Address;
+
 public class GameMaster {
+    private Profile exact;
     public String getBoard(){
         return null;
     }
-    public static void getPlayer(String username, Integer userid){
-        Profile x = new Profile(username, userid);
-        Integer totalPlayed = x.getTotalPlayed();
-        Integer wins = x.getWins();
-        Integer loses = x.getLosses();
+    public static void getPlayer(Profile prof){
+        String username = prof.getName();
+        Integer userid = prof.getID();
+        Integer totalPlayed = prof.getTotalPlayed();
+        Integer wins = prof.getWins();
+        Integer loses = prof.getLosses();
         System.out.println("Player: "+username+"\n" +"ID: "+userid+ "\n"+ "TotalPlay Count: "+totalPlayed+ "\n"+ "Win/Lose: "+wins+"/"+loses);
     }
     public static void main(String arg[]) {
-
-        getPlayer("name", 1);
+        ProfileManager pm = new ProfileManager();
+        getPlayer(pm.getProfileByName("Fillip"));
     }
 }
