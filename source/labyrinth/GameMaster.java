23d49983c0d8d97fe7d64805d6cdc5a15adc16cc
Matto <kingfok2000@gmail.come>
2020-11-22 23:06:30 +0900

    Update to get player data

--- source/labyrinth/GameMaster.java
+++ source/labyrinth/GameMaster.java
@@ -4,5 +4,12 @@ public class GameMaster {
     public String getBoard(){
         return null;
     }
+    public void getPlayer(String username, Integer userid){
+        Profile x = new Profile(username, userid);
+        Integer totalPlayed = x.getTotalPlayed();
+        Integer wins = x.getWins();
+        Integer loses = x.getLosses();
+        System.out.println("Player: "+username+"\n" +"ID: "+userid+ "\n"+ "TotalPlay Count: "+totalPlayed+ "\n"+ "Win/Lose: "+wins+"/"+loses);
+    }
 
 }
