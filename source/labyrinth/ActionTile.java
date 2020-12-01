c5c2a9400c8826dc3a2a580b50cfa92b964fc73f
Ian <ianl15@icloud.com>
2020-12-01 22:48:35 +0000

    Revert "Revert "Action tile class done, removed unnecessary comment in FileReader""
    This reverts commit 6f4d97bd0bd1a3509ba24080ef70c83010c1976a.

--- source/labyrinth/ActionTile.java
+++ source/labyrinth/ActionTile.java
@@ -1,26 +1,12 @@
 package source.labyrinth;
 
+//Action Tiles (Likely stored as an enum similar to our FloorTile class, should contain a file url to an icon of the Action Tile).
+
 public class ActionTile extends Tile {
-    public enum ActionType {
-        FIRE("source/resources/img/action_tile_fire2.png"),
-        ICE("source/resources/img/action_tile_ice2.png"),
-        DOUBLEMOVE("source/resources/img/action_tile_double_move.png"),
-        BACKTRACK("source/resources/img/action_tile_back_track.png");
-
-        public final String imageURL;
-
-        ActionType(String imageURL) {
-            this.imageURL = imageURL;
-        }
-    }
-
-    private final ActionType actionType;
-
-    public ActionTile(ActionType actionType) {
-        this.actionType = actionType;
-    }
-
-    public ActionType getType() {
-        return this.actionType;
+    public enum TileType{
+        FIRE,
+        FREEZE,
+        DOUBLEMOVE,
+        BACKTRACK
     }
 }
