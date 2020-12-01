06264d094aa5731e0855ed3d6298f3e675145183
Ian <ianl15@icloud.com>
2020-12-01 23:02:39 +0000

    Auto stash before rebase of "origin/master"
    Idk what happened before, I think i deleted it all but anyways here is the action tile class

--- source/labyrinth/ActionTile.java
+++ source/labyrinth/ActionTile.java
@@ -1,10 +1,17 @@
 package source.labyrinth;
 
 public class ActionTile extends Tile {
-    public enum TileType{
-        FIRE,
-        FREEZE,
-        DOUBLEMOVE,
-        BACKTRACK
+    public enum ActionType {
+        FIRE("../resources/img/action_tile_fire2.png"),
+        ICE("../resources/img/action_tile_ice2.png"),
+        DOUBLEMOVE("../resources/img/action_tile_double_move.png"),
+        BACKTRACK("../resources/img/action_tile_back_track.png");
+
+
+        private final String imageURL;
+
+        ActionType(String imageURL) {
+            this.imageURL = imageURL;
+        }
     }
 }
