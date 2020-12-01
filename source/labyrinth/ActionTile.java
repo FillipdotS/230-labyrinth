b78ccb2eef2bb2200db2088645ee5ff8d34733aa
Ian <ianl15@icloud.com>
2020-12-01 22:43:26 +0000

    Action tile class done, removed unnecessary comment in FileReader

--- source/labyrinth/ActionTile.java
+++ source/labyrinth/ActionTile.java
@@ -1,10 +1,21 @@
 package source.labyrinth;
 
+//Action Tiles (Likely stored as an enum similar to our FloorTile class, should contain a file url to an icon of the Action Tile).
+
 public class ActionTile extends Tile {
-    public enum TileType{
-        FIRE,
-        FREEZE,
-        DOUBLEMOVE,
-        BACKTRACK
+    public enum ActionType{
+        FIRE("../resources/img/action_tile_fire2.png"),
+        ICE("../resources/img/action_tile_ice2.png"),
+        DOUBLEMOVE("../resources/img/action_tile_double_move.png"),
+        BACKTRACK("../resources/img/action_tile_back_track.png");
+
+
+        private final String imageURL;
+
+        ActionType(String imageURL) {
+            this.imageURL = imageURL;
+        }
     }
+
+
 }
