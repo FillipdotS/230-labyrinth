e19f8de35941b445f3c825c00ef215e782fbbcab
Erik <miller.erik.1998@gmail.com>
2020-12-02 23:50:51 +0000

    Auto stash before rebase of "origin/master"

--- source/labyrinth/ActionTile.java
+++ source/labyrinth/ActionTile.java
@@ -2,7 +2,7 @@ package source.labyrinth;
 
 public class ActionTile extends Tile {
     public enum ActionType {
-        FIRE("source/resources/img/action_tile_fire2.png"),
+        FIRE("source/resources/img/action_tile_fire.png"),
         ICE("source/resources/img/action_tile_ice.png"),
         DOUBLEMOVE("source/resources/img/action_tile_double_move.png"),
         BACKTRACK("source/resources/img/action_tile_back_track.png");
