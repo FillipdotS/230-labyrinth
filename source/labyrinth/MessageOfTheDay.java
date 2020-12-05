a72723c7a7e58f707c999de35ec188c09f25c51c
Ian <ianl15@icloud.com>
2020-12-05 02:11:36 +0000

    Just cleaned it up

--- source/labyrinth/MessageOfTheDay.java
+++ source/labyrinth/MessageOfTheDay.java
@@ -13,10 +13,6 @@ public class MessageOfTheDay {
     private static final char[] alphabet = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
 
     public static void main(String[] args) throws IOException {
-        //sendGET(GET_URL);
-        //sendGET(GET_MSG_OF_THE_DAY);
-        //System.out.println(solvePuzzle("AZZ", 2));
-        //System.out.println(solvePuzzleTwo("CAB"));
         System.out.println(getPuzzleMessage());
     }
 
@@ -41,7 +37,6 @@ public class MessageOfTheDay {
 
             // print result
             finalResponse = buffer.toString();
-            System.out.println("Got this from request: " + finalResponse);
         } else {
             System.out.println("GET request not worked");
         }
@@ -57,46 +52,17 @@ public class MessageOfTheDay {
         return -1;
     }
 
-    private static String solvePuzzle(String puzzle, int shift) {
+    private static String solvePuzzle(String puzzle) {
         String answer = "";
-        for (int x = 1; x <= puzzle.length(); x++) {
-            char c = (puzzle.charAt(x - 1));
-            if (x % 2 == 0) {
-                System.out.println("Works?");
-                if (c > 'Z') {
-                    answer += (char) (puzzle.charAt(x - 1) - (23 - shift));
-                } else {
-                    answer += (char) (puzzle.charAt(x - 1) + shift);
-                }
-            } else {
-                if (c < 'A') {
-                    System.out.println("works2");
-                    answer += (char) (puzzle.charAt(x - 1) + (23 + shift));
-                } else {
-                    System.out.println("works3");
-                    answer += (char) (puzzle.charAt(x - 1) - shift);
-                }
-            }
-        }
-        return answer;
-    }
-
-    private static String solvePuzzleTwo(String puzzle) {
-        String answer = "";
-        System.out.println("Starting a solve, got given: " + puzzle);
 
         for (int i = 0; i < puzzle.length(); i++) {
             char currentChar = puzzle.charAt(i);
             int currentCharPos = alphabetPos(currentChar);
             int shiftAmount = i + 1;
             boolean shiftingBackwards = i % 2 == 0;
-
-            System.out.println("Letter: " + currentChar + " with value " + currentCharPos + " will be shifted by " + shiftAmount + " and shiftingBackwards is " + shiftingBackwards);
-
             int resultingLetterPosition;
 
             if (shiftingBackwards) {
-                System.out.println("Shifting backwards");
                 resultingLetterPosition = (currentCharPos - shiftAmount) % alphabet.length;
                 if (resultingLetterPosition > -1) {
                     answer += alphabet[resultingLetterPosition];
@@ -104,18 +70,12 @@ public class MessageOfTheDay {
                     answer += alphabet[alphabet.length + resultingLetterPosition];
                 }
             } else {
-                System.out.println("Shifting forwards");
                 resultingLetterPosition = (currentCharPos + shiftAmount) % alphabet.length;
                 answer += alphabet[resultingLetterPosition];
             }
-
-            System.out.println("Shifted " + currentChar + " to " + answer.charAt(i));
         }
-
-        System.out.println(answer);
         answer = "CS-230" + answer;
         answer += answer.length();
-        System.out.println(answer);
         return answer;
     }
 
@@ -123,13 +83,12 @@ public class MessageOfTheDay {
         String endResult = "";
         try {
             String givenPuzzle = sendGET(GET_URL);
-            String solvedPuzzle = solvePuzzleTwo(givenPuzzle);
+            String solvedPuzzle = solvePuzzle(givenPuzzle);
             endResult = sendGET(GET_MSG_OF_THE_DAY + solvedPuzzle);
         } catch (IOException e) {
             e.printStackTrace();
             System.exit(0);
         }
-
         return endResult;
     }
 }
